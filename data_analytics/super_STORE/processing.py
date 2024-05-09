5f92c6c0fdbaa0e5889e4ccf24dfec3c7b675692
Keneth M <mkn.keneth@gmail.com>
2024-05-09 17:26:22 +0300

    UPDATE: Defining consistent date data_type and also associating column to the correct data_type

--- data_analytics/super_STORE/processing.py
+++ data_analytics/super_STORE/processing.py
@@ -26,3 +26,41 @@ CUSTOMER_DETAILS = ['customer_name', 'segment', 'city', 'state',
 
 PRODUCT_DETAILS = ['product_id', 'category', 'sub-category', 'product_name',
                    'sales', 'quantity', 'discount', 'profit', 'shipping_cost']
+
+# data cleaning for the whole dataset
+# ## Setting similar date for the transaction dates
+orders_dataset = dataset.select(pl.col(ORDER_DETAILS))
+order_date = orders_dataset.select(pl.col('order_date'))
+ship_date = orders_dataset.select(pl.col('ship_date'))
+
+
+def date_convert(date_list):
+    data_return = []
+    for set_data in date_list:
+        if str(set_data).__contains__("-"):
+            data_return.append(datetime.strptime(set_data, "%d-%m-%y"))
+        elif str(set_data).__contains__("/"):
+            data_return.append(datetime.strptime(set_data, "%d/%m/%Y"))
+    return data_return
+
+
+order_list = (eval(str(list(order_date
+                            .to_dict(as_series="")
+                            .values()))
+                   .replace("[[", "[")
+                   .replace("]]", "]")))
+
+ship_list = (eval(str(list(ship_date
+                           .to_dict(as_series="")
+                           .values()))
+                  .replace("[[", "[")
+                  .replace("]]", "]")))
+
+order_and_ship_df = pl.DataFrame({"order_date": date_convert(order_list),
+                                  "ship_date": date_convert(ship_list)})
+
+order_dataset = order_and_ship_df.with_columns(orders_dataset
+                                               .select(pl.col(['order_id',
+                                                               'ship_mode',
+                                                               'order_priority'
+                                                               ])))
