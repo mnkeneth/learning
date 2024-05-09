3b239a215c516144b7066b589e216ca3ac85bf8b
Keneth M <mkn.keneth@gmail.com>
2024-05-09 17:24:39 +0300

    UPDATE: defining simple lower case column headers and replacing spaces with underscore i.e. _

--- data_analytics/super_STORE/processing.py
+++ data_analytics/super_STORE/processing.py
@@ -10,3 +10,9 @@ FULL_PATHNAME = PARENT_PATH / FILE_NAME
 
 dataset = pl.read_parquet(FULL_PATHNAME.resolve())
 
+# Renaming dataset column header
+new_column_name = (str(dataset.columns).
+                   lower().replace(" ", "_").
+                   replace(",_", ", "))
+
+dataset.columns = eval(new_column_name)
