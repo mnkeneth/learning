b55e644b31b2ece657b28e427060ecef2f887ca1
Keneth M <mkn.keneth@gmail.com>
2024-05-11 11:43:33 +0300

    UPDATE: Listing the top most profitable countries

--- data_analytics/super_STORE/analytics.py
+++ data_analytics/super_STORE/analytics.py
@@ -11,4 +11,32 @@ WORKING_FILE_NAME = "wrking_Superstore.parquet"
 WORKING_DATASET = PARENT_PATH / WORKING_FILE_NAME
 
 dataset = pl.read_parquet(WORKING_DATASET.resolve())
-print(dataset.head(10))
+
+# Finacial data reports.
+# # Total sales by country
+
+COLUMNS = ['country', 'sales', 'discount', 'profit']
+sales_dataset = dataset.select(pl.col(COLUMNS))
+
+# Group by country
+sales_by_country = sales_dataset.groupby(
+                                 "country").agg(pl.col(
+                                                      'sales',
+                                                      'discount',
+                                                      'profit')
+                                                .sum())
+
+# Sorting by the profitable country
+sales_by_country = sales_by_country.sort(by=pl.col("profit"), descending=True)
+
+with pl.Config(
+     tbl_cell_numeric_alignment="RIGHT",
+     thousands_separator=True,
+     float_precision=2,
+     tbl_formatting="ASCII_MARKDOWN",
+     tbl_hide_column_data_types=True,
+     tbl_hide_dataframe_shape=True,
+     ):
+
+    print(sales_by_country.head(9))
+
