27cbb9f0b6b926d6433c2a170bee9ab5122e3fa9
Keneth M <mkn.keneth@gmail.com>
2024-05-15 21:23:42 +0300

    UPDATE: Importing required files

---
+++ data_analytics/super_STORE/reporting.py
@@ -0,0 +1,9 @@
+#! /home/v7por9/.pyenv/shims/python
+
+import polars as pl
+
+# Local Import
+from analytics import Location_Data
+from analytics import Product_Data
+from analytics import Market_Data
+
